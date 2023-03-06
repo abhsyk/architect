b2578b299b080edf5f665cc31a6efb847cf18763
Sho Harada <tmchtmtr09876@gmail.com>
2023-03-06 14:08:26 +0900

    Styling Scroll button

--- src/components/ui/ScrollButton/index.tsx
+++ src/components/ui/ScrollButton/index.tsx
@@ -1,23 +1,59 @@
-import { FC } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { FC, useEffect, useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const ScrollButton: FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled upto given distance
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 800) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Set the top coordinate to 0
+  // make scrolling smooth
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  // the scroll event fires when the document view has been scrolled
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+  }, []);
+
   return (
-    <Button>
-      <FaArrowUp />
-    </Button>
+    <AnimatePresence>
+      {isVisible && (
+        <Button
+          onClick={scrollToTop}
+          variants={scrollAnim}
+          initial="initial"
+          animate="animate"
+          exit="initial"
+        >
+          <FaArrowUp />
+        </Button>
+      )}
+    </AnimatePresence>
   );
 };
 
 // Styles
-const Button = styled.a`
+const Button = styled(motion.div)`
   position: fixed;
-  right: 2rem;
-  bottom: 2rem;
+  right: 2.5rem;
+  bottom: 2.5rem;
   color: ${({ theme }) => theme.colors.white};
-  width: 2.5rem;
-  height: 2.5rem;
+  width: 3rem;
+  height: 3rem;
   background: ${({ theme }) => theme.colors.primary};
   display: flex;
   justify-content: center;
@@ -27,5 +63,17 @@ const Button = styled.a`
   box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
   cursor: pointer;
 `;
+
+// Animation variants
+const scrollAnim = {
+  initial: { y: '.5rem', opacity: 0 },
+  animate: {
+    y: '0rem',
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 export default ScrollButton;
