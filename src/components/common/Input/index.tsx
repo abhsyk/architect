431e70bcee9b9a4497f6bf273f20cb9ff7802357
Sho Harada <tmchtmtr09876@gmail.com>
2023-03-07 18:40:17 +0900

    Split file for Input

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,9 +1,8 @@
 import { FC, InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
-import styled from 'styled-components';
-import { respondTo } from '../../../styles';
+import { StyledInput } from './styles';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  // TextareaHTMLAttributes<HTMLTextAreaElement>
+interface Props
+  extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
   label: string;
   textarea?: boolean;
 }
@@ -12,7 +11,7 @@ const Input: FC<Props> = ({ label, textarea, ...rest }) => {
   return (
     <StyledInput>
       {textarea ? (
-        <textarea className="field" placeholder=" "></textarea>
+        <textarea {...rest} className="field" placeholder=" "></textarea>
       ) : (
         <input {...rest} className="field" placeholder=" " />
       )}
@@ -20,82 +19,5 @@ const Input: FC<Props> = ({ label, textarea, ...rest }) => {
     </StyledInput>
   );
 };
-
-// Styles
-const StyledInput = styled.div`
-  position: relative;
-
-  .field {
-    width: 25rem;
-    background-color: transparent;
-    border: none;
-    border-bottom: 0.2rem dashed #636363;
-    margin: 1.5rem 0;
-    padding: 1rem 1rem 1rem 0;
-    font-family: 'Muli', serif;
-    font-size: 1.2rem;
-    color: #4b4b4b;
-
-    @media ${respondTo.xl} {
-      margin: 1.4rem 0;
-      width: 32rem;
-    }
-    @media ${respondTo.lg} {
-      width: 28rem;
-    }
-    @media ${respondTo.md} {
-      width: 24rem;
-    }
-    @media ${respondTo.sm} {
-      width: 18rem;
-    }
-
-    :focus {
-      border-bottom-style: solid;
-    }
-  }
-
-  input {
-    height: 2rem;
-
-    :focus ~ label,
-    :not(:placeholder-shown) ~ label {
-      transform: translateY(1.9rem);
-      font-size: 0.8rem;
-
-      @media ${respondTo.lg} {
-        transform: translateY(2.2rem);
-      }
-    }
-  }
-
-  textarea {
-    height: 3rem;
-    resize: none;
-    overflow: hidden;
-
-    :focus ~ label,
-    :not(:placeholder-shown) ~ label {
-      transform: translateY(1.7rem);
-      font-size: 0.8rem;
-
-      @media ${respondTo.lg} {
-        transform: translateY(2.2rem);
-      }
-    }
-  }
-
-  label {
-    position: absolute;
-    left: 0;
-    font-family: 'Baloo Da 2', serif;
-    font-size: 1.2rem;
-    color: #4b4b4b;
-    text-transform: uppercase;
-    pointer-events: none;
-    bottom: 2rem;
-    transition: all 0.3s;
-  }
-`;
 
 export default Input;
